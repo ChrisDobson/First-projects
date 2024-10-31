0b3b52ee7cdb9e9b52d0aafb46b764ce81ffda77
SirhcPaul <christopher.dobson14@gmail.com>
2024-10-31 21:54:14 +0000

    added robots

--- 3 age.py
+++ 3 age.py
@@ -1,5 +1,5 @@
-#This is the corrected version from step 4
-
 age = input("How old are you?")
 ageNextYear = int(age) + 1
 print("You will be", ageNextYear, "next year. Happy birthday!")
+
+#INCORRECT CODE: ageNextYear = age + 1
---
+++ 4 robots.py
@@ -0,0 +1,11 @@
+user_reply = input ("Do you like robots? (Type yes, no, maybe or only daleks)")  
+if user_reply == "yes":
+    print ("Beep boop!")
+elif user_reply == "maybe":
+    print ("Please don't sit on the fence.")
+elif user_reply == "only daleks":
+    print ("EXTERMINATE")
+elif user_reply == "no":
+    print ("That's a shame!")
+else:
+    print ("What?")
